1dceab442fa77b60c5eef8beee179b6f13dc331c
imNaasir <cnaasiryaasiin7887@gmail.com>
2025-01-22 09:17:52 +0300

    first commit

---
+++ App.tsx
@@ -0,0 +1,21 @@
+import "./global.css";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function App() {
+  return (
+    <View className="flex-1 items-center justify-center bg-blue-100">
+      <Text className="text-red-500 font-bold">Hello NativeWind!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
